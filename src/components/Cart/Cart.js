748c91c5a3dc926b9714f467f047869839380ac0
faisalcep <faisalcep@yahoo.com>
2020-08-23 00:27:41 -0400

    firstCommit

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+const Cart = (props) => {
+  const cart = props.cart;
+  const totalPrice = cart.reduce((total, prd) => total + prd.price, 0);
+
+  //   let total = 0;
+  //   for (let i = 0; i < cart.length; i++) {
+  //     const product = cart[i];
+  //     total = total + product.price;
+  //   }
+
+  let shipping = 0;
+  if (totalPrice > 35) {
+    shipping = 0;
+  } else if (totalPrice > 15) {
+    shipping = 4.99;
+  } else if (totalPrice > 0) {
+    shipping = 12.99;
+  }
+
+  const tax = (totalPrice * 10) / 100;
+  const grandTotal = totalPrice + shipping + tax;
+
+  const formatNumber = (num) => {
+    const precision = num.toFixed(2);
+    return Number(precision);
+  };
+
+  return (
+    <div>
+      <h4>Order Summary</h4>
+      <p>Items Ordered: {cart.length}</p>
+      <p>Product Price: {formatNumber(totalPrice)}</p>
+      <p>
+        <small>Shipping Cost: {formatNumber(shipping)}</small>
+      </p>
+      <p>
+        <small>Tax: {formatNumber(tax)}</small>
+      </p>
+      <p>Total Price: {formatNumber(grandTotal)}</p>
+    </div>
+  );
+};
+
+export default Cart;
